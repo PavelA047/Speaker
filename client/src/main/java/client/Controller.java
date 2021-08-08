6d5660db36bbb34343e3a52a9d2c75d9a3098eb3
pavel <zzzzfogzzzz@gmail.com>
2021-08-09 01:06:45 +0600

    hw_7

--- client/src/main/java/client/Controller.java
+++ client/src/main/java/client/Controller.java
@@ -1,10 +1,16 @@
 package client;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -15,9 +21,17 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
-    public TextField textField;
+    private TextField textField;
     @FXML
-    public TextArea textArea;
+    private TextArea textArea;
+    @FXML
+    private TextField loginField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private HBox authPanel;
+    @FXML
+    private HBox msgPanel;
 
     private Socket socket;
     private final int PORT = 8189;
@@ -25,35 +39,29 @@ public class Controller implements Initializable {
     private DataInputStream in;
     private DataOutputStream out;
 
+    private boolean authenticated;
+    private String nick;
+    private Stage stage;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        try {
-            socket = new Socket(IP_ADDRESS, PORT);
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-            new Thread(() -> {
-                try {
-                    while (true) {
-                        String string = in.readUTF();
-                        if (string.equals("/end")) {
-                            System.out.println("Client disconnected");
-                            break;
+        Platform.runLater(() -> {
+            stage = (Stage) textField.getScene().getWindow();
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    System.out.println("bye");
+                    if (socket != null && !socket.isClosed()) {
+                        try {
+                            out.writeUTF("/end");
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
-                        textArea.appendText(string + "\n");
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        socket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
                 }
-            }).start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            });
+        });
+        setAuthenticated(false);
     }
 
     @FXML
@@ -65,5 +73,97 @@ public class Controller implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void tryToAuth(ActionEvent actionEvent) {
+        if (socket == null || socket.isClosed()) {
+            connect();
+        }
+        String log = loginField.getText().trim();
+        String pass = passwordField.getText().trim();
+        String msg = String.format("/auth %s %s", log, pass);
+
+        try {
+            out.writeUTF(msg);
+            loginField.clear();
+            passwordField.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setAuthenticated(boolean authenticated) {
+        this.authenticated = authenticated;
+        authPanel.setVisible(!authenticated);
+        authPanel.setManaged(!authenticated);
+        msgPanel.setVisible(authenticated);
+        msgPanel.setManaged(authenticated);
+
+        if (!authenticated) {
+            nick = "";
+        }
+        setTitle(nick);
+        textArea.clear();
+    }
+
+    public void connect() {
+        try {
+            socket = new Socket(IP_ADDRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String string = in.readUTF();
+                        if (string.startsWith("/")) {
+                            if (string.equals("/end")) {
+                                break;
+                            }
+                            if (string.startsWith("/authok")) {
+                                nick = string.split("\\s")[1];
+                                setAuthenticated(true);
+                                break;
+                            }
+                        } else {
+                            textArea.appendText(string + "\n");
+                        }
+                    }
+
+                    while (authenticated) {
+                        String string = in.readUTF();
+
+                        if (string.equals("/end")) {
+                            break;
+                        }
+                        textArea.appendText(string + "\n");
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("Disconnected");
+                    setAuthenticated(false);
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setTitle(String nickname) {
+        Platform.runLater(() -> {
+            if (!nickname.equals("")) {
+                stage.setTitle(String.format("Speaker [ %s ]", nickname));
+            } else {
+                stage.setTitle("Speaker");
+            }
+        });
     }
 }
